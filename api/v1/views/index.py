1fb8964b105331d91f2f478fbab392e4393aeb26
ayoubleass <ayoubleas@gmail.com>
2024-04-29 05:57:39 -0700

    fixing task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,12 +7,13 @@ This script containe the blueprint routes
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+import json
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """Returns a json response"""
-    return jsonify(status="ok")
+    return jsonify(status="OK")
 
 
 @app_views.route("/stats", strict_slashes=False)
