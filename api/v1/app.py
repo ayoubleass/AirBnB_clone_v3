929428d259367450802d1b42d1a7182e96f517d4
ayoubleass <ayoubleas@gmail.com>
2024-04-29 06:19:51 -0700

    fixing task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from os import getenv
 import json
 from flask import jsonify
+from werkzeug.exceptions import BadRequest
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -20,7 +21,7 @@ def close_storage(exception):
 def not_found(error):
     """Handler for 404 errors that returns a
     JSON-formatted 404 status code response"""
-    return jsonify(error="Not found")
+    return jsonify(error="Not found"), 400
 
 
 if __name__ == "__main__":
