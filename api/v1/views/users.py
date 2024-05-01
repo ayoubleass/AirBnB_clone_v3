440a3d3ef8077517562097fa203729a066fe2462
ayoubleass <ayoubleas@gmail.com>
2024-05-01 08:44:51 -0700

    fixing task 9

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -24,7 +24,7 @@ def show_user(user_id):
     return jsonify(user.to_dict())
 
 
-@app_views.route("/users/<user_id>",methods=['DELETE'], strict_slashes=False)
+@app_views.route("/users/<user_id>", methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """Delete a user """
     user = storage.get("User", user_id)
