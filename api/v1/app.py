982279b46356442c723faa0394fa0f529ef5d2bd
ayoubleass <ayoubleas@gmail.com>
2024-05-02 09:48:35 -0700

    adding task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,9 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask import jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
