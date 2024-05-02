349595732941a603a5ce9ad75003ea18a65fdf31
ayoubleass <ayoubleas@gmail.com>
2024-05-02 09:53:43 -0700

    fixing task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from os import getenv
 from flask import jsonify
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
